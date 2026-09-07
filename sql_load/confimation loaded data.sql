0a200d874861edfd130e942aee2cad16e1984c30
Cristian <cristianavsa@hotmail.com>
2026-09-07 17:23:05 -0600

    first commit

---
+++ sql_load/confimation loaded data.sql
@@ -0,0 +1,14 @@
+
+-- Confirmation of loaded data 
+SELECT COUNT(*) 
+FROM company_dim;
+
+SELECT COUNT(*) 
+FROM skills_dim;
+
+SELECT COUNT(*) 
+FROM job_postings_fact;
+
+SELECT COUNT(*) 
+FROM 
+skills_job_dim;
